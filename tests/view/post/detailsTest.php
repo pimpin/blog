3e57df90c2ca96a60a4d79dec54115e0301367b9
cdi-dl-mars <change_me@domain.com>
2014-03-04 15:59:36 +0000

    WIP

--- tests/view/post/detailsTest.php
+++ tests/view/post/detailsTest.php
@@ -1,17 +1,26 @@
 <?php
-include_once "/home/action/workspace/pierre/blog/src/Post.php";
-class detailsViewTest extends PHPUnit_Framework_TestCase
+class postDetailsViewTest extends PHPUnit_Framework_TestCase
 {
+  protected $post;
+
+  public function setUp()
+  {
+    
+    $this->post = new Post('Test titre2','Test contenu');
+  }
+
   public function testShowTitle()
   {
-    $post = new Post('Test titre2','Test contenu');
+    $post = $this->$this->getMockBuilder('Post')
+                     ->disableOriginalConstructor()
+                     ->getMock();
     $this->expectOutputRegex("/<h2>Test titre2<\/h2>/");
     include('/home/action/workspace/pierre/blog/src/view/post/details.phtml');
   }
   
-    public function testShowContent()
+  public function testShowContent()
   {
-    $post = new Post('Test titre2','Test contenu');
+    $post = $this->post;
     $this->expectOutputRegex("/Test contenu/");
     include('/home/action/workspace/pierre/blog/src/view/post/details.phtml');
   }
