8d020134c974c0bce614cbc181d2220708620572
cdi-dl-mars <change_me@domain.com>
2014-03-04 11:54:01 +0000

    Hello PhpUnit

---
+++ tests/PostTest.php
@@ -0,0 +1,10 @@
+<?php
+// include_once "/home/action/workspace/pierre/blog/src/Post.php";
+class PostTest extends PHPUnit_Framework_TestCase
+{
+  public function testGetTitle()
+  {
+  $first_post = new Post();
+  }
+}
+?>
