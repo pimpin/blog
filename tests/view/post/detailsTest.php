42f432f93da719f8dec29bfafe423aff4c5c0cf2
Pierre on nitrous Lannisport <pierrem@apido,org>
2014-03-05 12:39:22 +0000

    update Post detailsTest

--- tests/view/post/detailsTest.php
+++ tests/view/post/detailsTest.php
@@ -5,15 +5,17 @@ class postDetailsViewTest extends PHPUnit_Framework_TestCase
 
   public function setUp()
   {
-    
-    $this->post = new Post('Test titre2','Test contenu');
+    $this->post = $this->getMockBuilder('Post')
+                     ->disableOriginalConstructor()
+                     ->getMock();
   }
 
   public function testShowTitle()
   {
-    $post = $this->$this->getMockBuilder('Post')
-                     ->disableOriginalConstructor()
-                     ->getMock();
+    $post = $this->post;
+    $post->expects($this->any())
+      ->method('getTitle')
+      ->will($this->returnValue('Test titre2'));
     $this->expectOutputRegex("/<h2>Test titre2<\/h2>/");
     include('/home/action/workspace/pierre/blog/src/view/post/details.phtml');
   }
@@ -21,6 +23,10 @@ class postDetailsViewTest extends PHPUnit_Framework_TestCase
   public function testShowContent()
   {
     $post = $this->post;
+    $post->expects($this->any())
+      ->method('getContent')
+      ->will($this->returnValue('Test contenu'));
+    $this->expectOutputRegex("/<h2>Test titre2<\/h2>/");
     $this->expectOutputRegex("/Test contenu/");
     include('/home/action/workspace/pierre/blog/src/view/post/details.phtml');
   }
