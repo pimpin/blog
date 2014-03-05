f2e14035a4504dab10682b9bfe066bf9f71d2ac2
Pierre on nitrous Lannisport <pierrem@apido,org>
2014-03-05 15:08:02 +0000

    Add test coverage for exception
    max length title exception on Post

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -1,17 +1,25 @@
 <?php
-// include_once "/home/action/workspace/pierre/blog/src/Post.php";
+//include_once "/home/action/workspace/pierre/blog/src/Post.php";
 class PostTest extends PHPUnit_Framework_TestCase
 {
   public function testGetTitle()
   {
-  $post = new Post('Titre test', '');
-  $this->assertEquals($post->getTitle(), 'Titre test');
+    $post = new Post('Titre test', '');
+    $this->assertEquals($post->getTitle(), 'Titre test');
   }
   
   public function testGetContent()
   {
-  $post = new Post('','Contenu test');
-  $this->assertEquals($post->getContent(), 'Contenu test');
+    $post = new Post('','Contenu test');
+    $this->assertEquals($post->getContent(), 'Contenu test');
+  }
+  
+  public function testException()
+  {
+    $this->setExpectedException('InvalidArgumentException');
+    
+    $title_too_long = 'Too long title that is not accepted by business which ask to limit our title size';
+    $post = new Post($title_too_long,'Contenu test');
   }
 }
 ?>
