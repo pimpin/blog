c4340b9e1fb6ad796d036cc87de4d20fc49ee7bd
Jade <Jade@Jade-HP.mshome.net>
2014-03-06 15:14:18 +0100

    save

--- tests/RecetteTest.php
+++ tests/RecetteTest.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once '../src/Recipe.php';
+require_once dirname(__FILE__)."..\src\Recipe.php";
 
 class RecipeTest extends PHPUnit_Framework_TestCase {
 
     public function testGetCategory() {
         $entree = new recetteCree("homard_mayonnais", "homard");
         $this->assertEquals($post->getTitle(), 'Titre test');
+        
     }
 
     public function testGetIngrdients() {
