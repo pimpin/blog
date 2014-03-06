f8534c5e6bbb9045896678e84c74a5f73fe83f26
Nono <Nono@172.26.10.44>
2014-03-06 11:50:35 +0100

    creation place.php et modif des variable dans placetest.php

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -1,33 +1,33 @@
 <?php
+
 //include_once "/home/action/workspace/pierre/blog/src/Post.php";
-class PostTest extends PHPUnit_Framework_TestCase
-{
-  public function testGetTitle()
-  {
-    $post = new Post('Titre test', '');
-    $this->assertEquals($post->getTitle(), 'Titre test');
-  }
-  
-  public function testGetContent()
-  {
-    $post = new Post('','Contenu test');
-    $this->assertEquals($post->getContent(), 'Contenu test');
-  }
-  
-  public function testExceptionOnTooLongTitle()
-  {
-    $this->setExpectedException('InvalidArgumentException');
-    
-    $title_too_long = 'Too long title that is not accepted by business which ask to limit our title size';
-    $post = new Post($title_too_long,'Contenu test');
-  }
-  
-  public function testExceptionOnTooLongTitleHasRightMessage()
-  {
-    $this->setExpectedException(
-      'InvalidArgumentException', '$title must be under 40 characteres'
-    );
-    $title_too_long = 'Too long title that is not accepted by business which ask to limit our title size';
-    $post = new Post($title_too_long,'Contenu test');  }
+class PostTest extends PHPUnit_Framework_TestCase {
+
+    public function testGetTitle() {
+        $post = new Post('Titre test', '');
+        $this->assertEquals($post->getTitle(), 'Titre test');
+    }
+
+    public function testGetContent() {
+        $post = new Post('', 'Contenu test');
+        $this->assertEquals($post->getContent(), 'Contenu test');
+    }
+
+    public function testExceptionOnTooLongTitle() {
+        $this->setExpectedException('InvalidArgumentException');
+
+        $title_too_long = 'Too long title that is not accepted by business which ask to limit our title size';
+        $post = new Post($title_too_long, 'Contenu test');
+    }
+
+    public function testExceptionOnTooLongTitleHasRightMessage() {
+        $this->setExpectedException(
+                'InvalidArgumentException', '$title must be under 40 characteres'
+        );
+        $title_too_long = 'Too long title that is not accepted by business which ask to limit our title size';
+        $post = new Post($title_too_long, 'Contenu test');
+    }
+
 }
+
 ?>
